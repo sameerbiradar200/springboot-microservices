ad308cce697ac4300568ea429ca57b12c81f2c96
sameer <biradarsamer2@gmail.com>
2025-06-28 00:46:04 +0530

    Initial commit

---
+++ api-gateway-service/src/main/java/com/sameer/gateway/api_gateway_service/Test.java
@@ -0,0 +1,21 @@
+package com.sameer.gateway.api_gateway_service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class Test {
+
+    @Bean
+    public RouterFunction<ServerResponse> route() {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/"), req -> ServerResponse.ok().bodyValue("Hello from API Gateway Service!"));
+    }
+}
---
+++ user-service/src/main/java/com/sameer/user/user_service/controller/Test.java
@@ -0,0 +1,14 @@
+package com.sameer.user.user_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello from User Service!";
+    }
+}
